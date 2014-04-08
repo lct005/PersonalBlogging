4c44cc25f87ab11c070727783aacb90506cccb63
lct005 <lct_005@yahoo.com.vn>
2014-04-08 14:50:11 +0700

    Add project Nt.Common

---
+++ PersonalBlogging/Nt.Common/Constant/ConstParam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nt.Common.Constant
+{
+    public class ConstParam
+    {
+        public const string SystemCachingObject = "SystemCachingObject";
+
+        public const string RoleId = "RoleId";
+        public const string RoleName = "RoleName";
+        public const string UserId = "UserId";
+        public const string FullName = "FullName";
+        public const string LoginName = "LoginName";
+    }
+}
---
+++ PersonalBlogging/Nt.Common/Paging/IPagedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nt.Common.Paging
+{
+    public interface IPagedList
+    {
+        int CurrentPageIndex { get; set; }
+        int PageSize { get; set; }
+        int TotalItemCount { get; set; }
+        int StartRecordIndex { get; }
+        int EndRecordIndex { get; }
+    }
+}
